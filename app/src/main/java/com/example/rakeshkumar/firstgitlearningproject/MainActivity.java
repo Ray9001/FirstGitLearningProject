f5b6ad473a228acbc14163a7cca6e74427efdd37
rakeshkumar <rakesh.kumar6@tesco.com>
2018-10-04 23:45:24 +0530

    restore added and resume also

--- app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
+++ app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.rakeshkumar.firstgitlearningproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
