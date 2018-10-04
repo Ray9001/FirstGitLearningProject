9e0ca0669486e2aebf4d56feec25ac53d5ef22c7
rakeshkumar <rakesh.kumar6@tesco.com>
2018-10-04 23:55:59 +0530

    first added for main origni

--- app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
+++ app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.rakeshkumar.firstgitlearningproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean shouldShowRequestPermissionRationale(@NonNull String permission) {
+        return super.shouldShowRequestPermissionRationale(permission);
     }
 }
