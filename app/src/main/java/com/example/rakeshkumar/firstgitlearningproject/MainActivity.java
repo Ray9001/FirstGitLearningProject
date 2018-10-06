5ce8a803dc812033eec35108dd505747c18d0156
rakeshkumar <rakesh.kumar6@tesco.com>
2018-10-06 22:44:47 +0530

    date 6 first commit

--- app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
+++ app/src/main/java/com/example/rakeshkumar/firstgitlearningproject/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.rakeshkumar.firstgitlearningproject;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,30 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-    }
-
-
-    @Override
-    public void finishActivity(int requestCode) {
-        super.finishActivity(requestCode);
-    }
-
-    @Override
-    public void finish() {
-        super.finish();
     }
 }
